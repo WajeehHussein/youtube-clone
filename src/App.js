c1217b25b2a3cc7ec611b8974393674728b526f5
WajeehHussein <wajehessam3@gmail.com>
2022-10-21 20:15:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter , Routes , Route} from 'react-router-dom';
 import { Box } from '@mui/system';
 
-import {Navbar ,Feed,VideoDetail,SearchFeed } from './components'
+import {Navbar ,Feed,VideoDetail,SearchFeed,ChannelDetail } from './components'
 const App = () => (
   
 <BrowserRouter>
@@ -11,7 +11,7 @@ const App = () => (
   <Routes>
     <Route path='/' exact element={ <Feed />} />
     <Route path='/video/:id' element={ <VideoDetail />} />
-    {/* <Route path='/channel/:id' element={ <ChannelDetail />} /> */}
+    <Route path='/channel/:id' element={ <ChannelDetail />} />
     <Route path='/search/:searchTerm' element={ <SearchFeed />} />
   </Routes>
 </Box>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,21 @@
-import { Box, Stack } from '@mui/system';
-import {VideoCard,ChannelCard} from './';
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
+import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = ({videos}) => (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-        {videos.map((item,idx) => (
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
         <Box key={idx}>
-            {item.id.videoId && <VideoCard video={item }/>}
-            {item.id.channelId && <ChannelCard channelDetail={item }/>}
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
-        ))}
+      ))}
     </Stack>
-)
+  );
+}
 
-export default Videos
+export default Videos;
